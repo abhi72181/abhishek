b0e2bfb27ef2a99b2df30bf8326ca19d39c93618
abhi72181 <abhihits0404@gmail.com>
2022-05-07 20:50:04 +0530

    i

---
+++ resturant/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { RestaurantDetails } from "./components/RestaurantDetails";
+function App() {
+  return (
+    <div className="App">
+      <RestaurantDetails />
+    </div>
+  );
+}
+
+export default App;
